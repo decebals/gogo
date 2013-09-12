5e9f6b49f1459f9804c33f701dec8f93f6838014
Decebal Suiu <decebal.suiu@gmail.com>
2013-09-12 14:19:01 +0300

    add variable substitution support

--- src/main/java/ro/fortsoft/gogo/ExtendedProperties.java
+++ src/main/java/ro/fortsoft/gogo/ExtendedProperties.java
@@ -17,4 +17,58 @@ public class ExtendedProperties extends Properties {
 		super(defaults);
 	}
 
+	/**
+	 * Retrieves the property's value. Also if the value contains variables (${propertyName}) 
+	 * then the variables are resolved. 
+	 */
+	@Override
+    public String getProperty(String key) {
+        String value = super.getProperty(key);
+        if (value != null) {
+        	value = evaluate(value);
+        }
+        
+        return value;
+    }
+    
+    @Override
+    public String getProperty(String key, String defaultValue) {
+        String value = super.getProperty(key);
+        if (value != null) {
+        	value = evaluate(value);
+        }
+        
+        if (value == null) {
+        	value = defaultValue;
+        }
+        
+        return value;
+    }
+    
+    
+    private String evaluate(String value) {
+        if ((value == null) || (value.length() < 4)) {
+            return value;
+        }
+        
+        StringBuilder result = new StringBuilder(value.length());
+        result.append(value);
+        int start = result.indexOf("${");
+        int end = result.indexOf("}", start + 2);
+        while ((start >= 0) && (end > start)) {
+            String paramName = result.substring(start + 2, end);
+            String paramValue = getProperty(paramName);
+            if (paramValue != null) {
+                result.replace(start, end + 1, paramValue);
+                start += paramValue.length();
+            } else {
+                start = end + 1;
+            }
+            start = result.indexOf("${", start);
+            end = result.indexOf("}", start + 2);
+        }
+        
+        return result.toString();
+    }
+    
 }
